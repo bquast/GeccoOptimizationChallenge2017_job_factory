a22c0167866d41ccad91dd57263fdeff33994243
SP Mohanty <sharada.mohanty@epfl.ch>
2017-05-08 07:51:11 +0200

    Add some comments about the evaluate and submit functions

--- workers.py
+++ workers.py
@@ -9,20 +9,36 @@ import time
 POOL = redis.ConnectionPool(host=config.redis_host, port=config.redis_port, db=0)
 
 def _evaluate(data, redis_conn, response_channel):
+    """
+        Takes a single list of params and computes the score based on BluePyOpt
+    """
     result = 0
     for k in range(10):
         percent_complete = k*1.0/100
         update_progress(redis_conn, response_channel, percent_complete, "Optional Message")
         result += k
-    return result
+    _result_object = {
+        "score" : result,
+    }
+    return _result_object
 
 def _submit(data, redis_conn, response_channel):
+    """
+        Takes a single list of params and computes the score based on BluePyOpt
+        Also generates the relevant Images, and uploads to a S3 bucket, and sends back the link
+    """
     result = 0
     for k in range(10):
         percent_complete = k*1.0/100
         update_progress(redis_conn, response_channel, percent_complete, "Optional Message")
         result += k
-    return result
+
+    _result_object = {
+        "score" : result,
+        "thumbnail" : "link-to-thumbnail-for-the-submission-on-the-leaderboard",
+        "image" : "link-to-image-for-the-submission-on-the-leaderboard"
+    }
+    return _result_object
 
 def job_execution_wrapper(data):
     redis_conn = redis.Redis(connection_pool=POOL)
