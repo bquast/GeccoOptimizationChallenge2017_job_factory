9696241d571fe29e45b3996ae45d321a20670e61
SP Mohanty <sharada.mohanty@epfl.ch>
2017-05-08 06:31:55 +0200

    Reorder functions for simplicity in defining functions

--- workers.py
+++ workers.py
@@ -21,18 +21,6 @@ def _evaluate(data, redis_conn, response_channel):
         result += k
     return result
 
-def evaluate_wrapper(data):
-    redis_conn = redis.Redis(connection_pool=POOL)
-    job = get_current_job()
-    response_channel = config.redis_namespace+"::job_response::"+job.id
-    # Register Job Running event
-    redis_conn.rpush(response_channel, job_running_template())
-    # Run the job
-    result = _evaluate(data, redis_conn, response_channel)
-    # Register Job Complete event
-    redis_conn.rpush(response_channel, job_complete_template(result))
-    return result
-
 def _submit(data, redis_conn, response_channel):
     result = 0
     for k in range(10):
@@ -43,6 +31,18 @@ def _submit(data, redis_conn, response_channel):
 
         redis_conn.rpush(response_channel, job_progress_update(_progress_update, _progress_update["message"]))
         result += k
+    return result
+
+def evaluate_wrapper(data):
+    redis_conn = redis.Redis(connection_pool=POOL)
+    job = get_current_job()
+    response_channel = config.redis_namespace+"::job_response::"+job.id
+    # Register Job Running event
+    redis_conn.rpush(response_channel, job_running_template())
+    # Run the job
+    result = _evaluate(data, redis_conn, response_channel)
+    # Register Job Complete event
+    redis_conn.rpush(response_channel, job_complete_template(result))
     return result
 
 def submit_wrapper(data):
