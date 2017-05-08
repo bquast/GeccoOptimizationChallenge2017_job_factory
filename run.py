75ecbc669aacceaf7c36b1638e72383610b8229d
SP Mohanty <sharada.mohanty@epfl.ch>
2017-05-08 06:37:00 +0200

    Remove dev nots

--- run.py
+++ run.py
@@ -63,38 +63,3 @@ if __name__ == "__main__":
     r = redis.Redis()
     client = Listener(r, [config.redis_namespace+'::enqueue_job'], config, JOB_QUEUE, POOL)
     client.start()
-
-# Subscribe to the namespace+commands channel
-# In case of a new job, enqueue job.
-# Use the job-id to keep adding responses to a redis list
-
-
-# """
-# A job_factory listens on the `commands` channel, and executes the challenge specific function
-# """
-# conn.enqueue("function_name", function_data)
-# # ..
-# # ...
-# # ....
-# # responds with :
-# {
-#     job_enqueue_state : crowdai.job_state.ENQUEUED
-#                       : crowdai.job_state.WAITING
-#                       : crowdai.job_state.RUNNING
-#                       : crowdai.job_state.COMPLETE
-#                       : crowdai.job_state.ERROR
-#                       : crowdai.job_state.TIMEOUT
-#     data : {
-#     }
-#     message : "MESSAGE"
-# }
-#
-#
-#
-# }
-# {
-#     job_enqueue_status: True #If the Job Enqueue went well
-#     job_enqueue_message: "" #Optional Message in case of job enqueue failure
-#     job_input_channel: namespace+"::"+"input_channel_name"
-#     job_output_channel: namespace+"::"+"output_channel_name"
-# }
